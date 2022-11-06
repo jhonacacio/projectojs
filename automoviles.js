1e9cc6804976e52ad7d1d735c7a34deb56fa84e2
jhonacacio <jhonmarioacacio2@gmail.com>
2022-11-06 17:22:21 -0300

    Dom

---
+++ automoviles.js
@@ -0,0 +1,100 @@
+const modelosAutos = [
+    {
+        id: 1,
+        modeloAuto: 'Tiggo 3',
+        tipoAuto: 'Suv',
+        anioAuto: 2020,
+        precioLista: 10990,
+        precioAuto: 8990,
+        marca: 'Chery',
+        img : '/public/images/1.jpeg'
+    },
+
+    {
+        id: 2,
+        modeloAuto: 'Durango',
+        tipoAuto: 'Suv',
+        anioAuto: 2020,
+        precioLista: 20990,
+        precioAuto: 16720,
+        marca: 'Dodge',
+        img : '/public/images/2.jpeg'
+    },
+
+    {
+        id: 3,
+        modeloAuto: 'C5 Aircross',
+        tipoAuto: 'Suv',
+        anioAuto: 2020,
+        precioLista: 21200,
+        precioAuto: 16720,
+        marca: 'Citroen',
+        img : '/public/images/3.jpeg'
+    },
+
+    {
+        id: 4,
+        modeloAuto: 'Ridgeline',
+        tipoAuto: 'Camioneta',
+        anioAuto: 2020,
+        precioLista:15000,
+        precioAuto: 9890,
+        marca: 'Honda',
+        img : '/public/images/4.jpeg'
+    },
+
+    {
+        id: 5,
+        modeloAuto: 'Wrangler',
+        tipoAuto: 'Camioneta',
+        anioAuto: 2020,
+        precioLista:16990,
+        precioAuto: 11990,
+        marca: 'Jeep',
+        img : '/public/images/5.jpeg'
+    },
+
+    {
+        id: 6,
+        modeloAuto: 'Gladiator',
+        tipoAuto: 'Camioneta',
+        anioAuto: 2019,
+        precioLista:18990,
+        precioAuto: 11990,
+        marca: 'Jeep',
+        img : '/public/images/6.jpeg'
+    },
+
+    {
+        id: 7,
+        modeloAuto: 'C-elysee',
+        tipoAuto: 'Sedan',
+        anioAuto: 2021,
+        precioLista: 10990,
+        precioAuto: 8890,
+        marca: 'Citroen',
+        img : '/public/images/7.jpeg'
+    },
+
+    {
+        id: 8,
+        modeloAuto: 'Cronos',
+        tipoAuto: 'Sedan',
+        anioAuto: 2021,
+        precioLista:12990,
+        precioAuto: 9960,
+        marca: 'Fiat',
+        img : '/public/images/8.jpeg'
+    },
+
+    {
+        id: 9,
+        modeloAuto: 'Accord',
+        tipoAuto: 'Sedan',
+        anioAuto: 2022,
+        precioLista: 12990,
+        precioAuto: 10990,
+        marca: 'Honda',
+        img : '/public/images/9.jpeg'
+    },
+];
---
+++ comprobaciones.js
@@ -0,0 +1,5 @@
+function comprobacionTexto() {
+    let comprobacionModelo = prompt('Ingrese el modelo de auto o salir para cerrar la aplicación');
+    let comprobacionModeloMayuscula = comprobacionModelo.toLocaleUpperCase();
+    return comprobacionModeloMayuscula
+   }
